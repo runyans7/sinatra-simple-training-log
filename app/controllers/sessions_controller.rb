a4d01a8e37362917ff984a818d603ec820ee04e6
Scotty Runyan <runyans7@gmail.com>
2016-01-14 10:02:43 -0500

    Users can login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  get '/login' do
+    if logged_in?
+      redirect '/'
+    else
+      erb :'sessions/new'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      erb :'sessions/new'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
+end
