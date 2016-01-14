5eea312e43d048d7f187005b5ff597dd48469747
Scotty Runyan <runyans7@gmail.com>
2016-01-14 09:09:43 -0500

    Added bootstrap to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    "Hello world"
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
   end
-  
+
+  get '/' do
+    erb :'home'
+  end
+
+
 end
